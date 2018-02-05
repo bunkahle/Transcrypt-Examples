9fb67c8579d009fc04e06f33e4826aa067a01296
bunkahle <bunkahle@users.noreply.github.com>
2018-02-05 16:45:27 +0100

    Delete window2.py

--- dom/window2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-def change_background():
-    # change the background color to red
-	document.body.style.background = "red"
-	col = prompt("Which background color do you like?", "")
-	# change it back after 1 second
-	document.body.style.background = col
