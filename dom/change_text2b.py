d71ddf45794fe12d850f89531fab446278accb9a
bunkahle <bunkahle@users.noreply.github.com>
2018-02-07 00:29:48 +0100

    Add files via upload

---
+++ dom/change_text2b.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def insert():
+    myElement = document.getElementById("intro")
+    document.getElementById("demo").innerHTML = "The text from the intro paragraph is " + myElement.innerHTML
