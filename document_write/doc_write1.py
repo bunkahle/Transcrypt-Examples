d78f3b6add1d091a3273ec072ddc603097a8978a
bunkahle <bunkahle@users.noreply.github.com>
2018-02-07 00:26:52 +0100

    Add files via upload

---
+++ document_write/doc_write1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class TestSystem:
+    
+    def __init__ (self):
+        self.header = """<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>DocWrite</title>
+  </head>
+  <body>\n"""
+        self.footer = "\n  </body>\n</html>"
+    
+    def write(self):
+        document.write(self.header)
+        document.write("Hello World!")
+        document.write("Have a nice day!")
+        document.write("<p>Note that writeln() adds a new line after each statement:</p>")
+        document.writeln("Hello World!<br>")
+        document.writeln("Have a nice day!<br>")
+        document.write("Check the sourcecode for line breaks!")
+        document.write('    <button onclick="window.history.back();">Go Back</button>')
+        document.write(self.footer)
+
+testSystem = TestSystem()
