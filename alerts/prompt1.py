15470d1cc1d4cddcb402045aa31d218f448664d0
bunkahle <bunkahle@users.noreply.github.com>
2018-02-07 10:29:17 +0100

    Add files via upload

---
+++ alerts/prompt1.py
@@ -0,0 +1,23 @@
+class TestSystem:
+    
+    def __init__ (self):
+        self.testMessage = 'TestMessage'
+
+    def prompt_text(self):
+        entry = prompt('Please enter any text','')
+        if entry == "":
+            alert("Nothing has been entered")
+        if entry is None:
+            alert("None")
+        alert("Entry was:"+entry)
+        return entry
+
+    def prompt_test(self):
+        ret = self.prompt_text()
+        document.getElementById("result").innerHTML = "Result of entry:"+ret
+
+    def greet_name(self):
+        name = prompt('What is your name?','Transcrypt-User')
+        alert('Hallo, ' + name)
+        
+testSystem = TestSystem()
