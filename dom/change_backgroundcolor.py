e6cb3a02271930c75729501e538d205b413bf094
bunkahle <bunkahle@users.noreply.github.com>
2018-02-10 02:36:00 +0100

    Update change_backgroundcolor.py

--- dom/change_backgroundcolor.py
+++ dom/change_backgroundcolor.py
@@ -5,5 +5,4 @@ def change_background():
     # change the background color to red
 	document.body.style.background = "red"
 	col = prompt("Which background color do you like?", "")
-	# change it back after 1 second
 	document.body.style.background = col
