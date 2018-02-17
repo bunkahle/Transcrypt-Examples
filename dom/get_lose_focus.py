799beb3a6c7fb44219ad5982f7c250d265ae1581
bunkahle <bunkahle@users.noreply.github.com>
2018-02-17 15:16:29 +0100

    Add files via upload

---
+++ dom/get_lose_focus.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def getfocus():
+    document.getElementById("myAnchor").focus()
+
+def losefocus():
+    document.getElementById("myAnchor").blur()
