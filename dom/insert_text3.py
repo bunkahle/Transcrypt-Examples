8e56388637a19f44ce361159ffd5c9d26c3766e8
bunkahle <bunkahle@users.noreply.github.com>
2018-02-18 01:03:31 +0100

    Add files via upload

--- dom/insert_text3.py
+++ dom/insert_text3.py
@@ -4,3 +4,6 @@
 def insert():
     x = document.getElementById("myPre").innerHTML
     document.getElementById("demo").innerHTML = x
+    console.log("This message can only be seen in the console window - hit F12 to view it in the browser")
+    console.log("Console.log can be used for debugging, especially for variables like x.")
+    console.log("x has the value:"+x+": end of var")
