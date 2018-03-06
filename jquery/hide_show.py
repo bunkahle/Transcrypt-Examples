4d97a9ed5443de50ad83b41afb145e1cf59b9b8e
bunkahle <bunkahle@users.noreply.github.com>
2018-03-07 00:42:37 +0100

    Add files via upload

--- jquery/hide_show.py
+++ jquery/hide_show.py
@@ -4,22 +4,22 @@
 __pragma__ ('alias', 'S', '$')
 
 def install_hide_show():
-	S("#hide").click(lambda: 
-		S("p").hide()
-	)
-	S("#show").click(lambda: 
+    S("#hide").click(lambda: 
+        S("p").hide()
+    )
+    S("#show").click(lambda: 
         S("p").show()
     )
 
 """ original Javascript:
-	$(document).ready(function(){
-	    $("#hide").click(function(){
-	        $("p").hide();
-	    });
-	    $("#show").click(function(){
-	        $("p").show();
-	    });
-	});
+    $(document).ready(function(){
+        $("#hide").click(function(){
+            $("p").hide();
+        });
+        $("#show").click(function(){
+            $("p").show();
+        });
+    });
 """
 S(document).ready(install_hide_show)
     
