7b843e2309a582412f3f8da2f8499648397bf984
bunkahle <bunkahle@users.noreply.github.com>
2018-03-06 16:25:44 +0100

    Add files via upload

--- jquery/change_text.py
+++ jquery/change_text.py
@@ -4,9 +4,9 @@
 __pragma__ ('alias', 'S', '$')
 
 def insert():
-	text = "The text from the intro paragraph is "
-	S("#demo").html(text+S("#intro").html())
-	# instead of:
+    text = "The text from the intro paragraph is "
+    S("#demo").html(text+S("#intro").html())
+    # instead of:
     # myElement = document.getElementById("intro")
     # document.getElementById("demo").innerHTML = "The text from the intro paragraph is " + myElement.innerHTML
 
