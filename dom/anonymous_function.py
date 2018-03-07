42651d222fcbd5c8d7dc3591df38283d7e5e135c
bunkahle <bunkahle@users.noreply.github.com>
2018-03-07 08:55:24 +0100

    Add files via upload

---
+++ dom/anonymous_function.py
@@ -0,0 +1,9 @@
+x = lambda a, b: a * b
+document.getElementById("demo").innerHTML = x(4, 3);
+
+""" original Javascript:
+<script>
+var x = function (a, b) {return a * b};
+document.getElementById("demo").innerHTML = x(4, 3);
+</script>
+"""
